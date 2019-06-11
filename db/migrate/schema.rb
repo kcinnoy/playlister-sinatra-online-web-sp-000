aa72fc6525e16c0ea303d2d6352ecaecd07eeefb
Yonnick <yonnick00@gmail.com>
2019-06-11 12:55:27 +0000

    Automatically backed up by Learn

--- db/migrate/schema.rb
+++ db/migrate/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_034119) do
+ActiveRecord::Schema.define(version: 2019_06_11_034119) do
 
   create_table "artists", force: :cascade do |t|
     t.text "name"
