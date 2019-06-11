8d0b43df10eed9d16e33f192c6bd307294c6ca8b
Yonnick <yonnick00@gmail.com>
2019-06-11 15:00:37 +0000

    Automatically backed up by Learn

--- db/migrate/20190611143921_create_songs_genres.rb
+++ db/migrate/20190611143921_create_songs_genres.rb
@@ -1,4 +1,8 @@
 class CreateSongsGenres < ActiveRecord::Migration[5.2]
   def change
-  end
+     create_table :song_genres do |t|
+       t.integer :song_id
+       t.integer :genre_id
+     end
+   end
 end
