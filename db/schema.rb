257ce6ee3dd1e761ec0310dfa95090224504f557
Yonnick <yonnick00@gmail.com>
2019-06-11 11:40:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_113359) do
+ActiveRecord::Schema.define(version: 2019_06_11_113949) do
 
 end
